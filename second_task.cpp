2b09851f9beb752f086dd7e2c42ad6120fe60995
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-10 10:10:02 +0300

    Add files via upload
    второе задание

---
+++ second_task.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+	setlocale(0, "");
+	cout << "Введите число для проверки: " << endl;
+	int u_num;
+	cin >> u_num;
+
+	if (u_num > 0)
+	{
+		cout << "Число положительное :]" << endl;
+	}
+
+	else if (u_num < 0)
+	{
+		cout << "Число отицательное :[" << endl;
+	}
+
+	else if (u_num == 0) 
+	{
+		cout << "Число равно нулю :O" << endl;
+	}
+
+	return 0;
+}

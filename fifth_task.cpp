8201024d4199b4ae3976ab1f275eae487003a02f
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-10 10:26:30 +0300

    Add files via upload
    пятое задание

---
+++ fifth_task.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите число для проверки: " << endl;
+	int u_num;
+	cin >> u_num;
+
+	if (u_num % 3 == 0 && u_num % 5 == 0) 
+	{
+		cout << "Число является кратным 3 и 5 одновременно :]" << endl;
+	}
+
+	else 
+	{
+		cout << "Число не является кратным 3 и 5 одновременно :[" << endl;
+	}
+
+	return 0;
+}

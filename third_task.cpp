4bdc04435226216e008fdc57f115a1c71dcb72ae
AkuTheShark <89930916+AkuTheShark@users.noreply.github.com>
2023-10-10 10:38:38 +0300

    Add files via upload
    третье задание

---
+++ third_task.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+	setlocale(0, "");
+	cout << "Введите год для проверки: " << endl;
+	int u_num;
+	cin >> u_num;
+
+	if (u_num % 4 == 0)
+	{
+		cout << "Год является високосным :]" << endl;
+	}
+
+	else
+	{
+		cout << "Год является обычным, без приколов :[" << endl;
+	}
+
+	return 0;
+}
